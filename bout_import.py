927f1a99ff4b79bb15f8189a909e123e65aad4ea
Peter Hill <zed.three@gmail.com>
2016-01-07 15:19:29 +0000

    Pass filename to request_data(), tidy up comments

--- bout_import.py
+++ bout_import.py
@@ -1,21 +1,24 @@
-from boututils.datafile import DataFile
-import boutdata
-from glob import glob
+"""Paraview reader for BOUT++ data files
+
+===============
+2015 Peter Hill
+"""
+
 import os.path
 import numpy as np
-from vtk import vtkPoints, vtkStructuredGrid, vtkDoubleArray
+
+from boututils.datafile import DataFile
+import boutdata
+
+from vtk import vtkPoints
 from vtk.numpy_interface import algorithms as algs
 from vtk.numpy_interface import dataset_adapter as dsa
 from paraview import numpy_support
 
-def get_bout_datafiles(directory):
-    # Read bout datafiles
-    return sorted(glob(os.path.join(directory, "BOUT.dmp.*nc")))
-
 def set_output_timesteps(pipeline, timesteps):
     """Helper routine to set timestep info
     """
-    
+
     executive = pipeline.GetExecutive()
     out_info = executive.GetOutputInformation(0)
 
@@ -40,28 +43,19 @@ def get_update_timestep(pipeline):
 def set_output_extent(pipeline, nx, ny, nz):
     """Helper routine to set extent info
     """
-    
+
     executive = pipeline.GetExecutive()
     out_info = executive.GetOutputInformation(0)
 
-    # out_info.Remove(executive.WHOLE_EXTENT())
     out_info.Set(executive.WHOLE_EXTENT(), 0, nx-1, 0, ny-1, 0, nz-1)
-    # print(out_info)
 
 def request_info(pipeline, filename):
     """RequestInformation script
     """
 
-    # import PyQt4.QtGui
-    # filename = str(PyQt4.QtGui.QFileDialog.getOpenFileName(None, "Select a file..."))
     directory = os.path.dirname(filename)
 
-    # print("Requesting info")
-    # Get list of output files
-    # file_list = get_bout_datafiles(directory)
-
     # Read timesteps from first file
-    # print("Variables in file:")
     with DataFile(filename) as f:
         # Get timesteps
         t_array = f.read("t_array")
@@ -70,12 +64,9 @@ def request_info(pipeline, filename):
 
         # Get list of variables with at least 2 dimensions
         var_list = [var for var in f.list() if f.ndims(var) >= 2]
-        # print(var_list)
 
     # Tell paraview about the timesteps
     set_output_timesteps(pipeline, t_array)
-    # print("Timesteps:")
-    # print(t_array)
 
     # Get dataset sizes
     dx = boutdata.collect("dx", path=directory, info=False)
@@ -87,25 +78,12 @@ def request_info(pipeline, filename):
 
     return directory
 
-def get_update_timestep(pipeline):
-    """Returns the requested time value, or None if not present
-    """
-    executive = pipeline.GetExecutive()
-    out_info = executive.GetOutputInformation(0)
-    if out_info.Has(executive.UPDATE_TIME_STEP()):
-        return out_info.Get(executive.UPDATE_TIME_STEP())
-    else:
-        return None
-
-def request_data(pipeline):
+def request_data(pipeline, filename):
     """Read the datasets from the output files
     """
 
     directory = pipeline.directory
 
-    file_list = get_bout_datafiles(directory)
-    # print(file_list)
-    
     # Collect dx, dy, dz
     dx = boutdata.collect("dx", path=directory, info=False)
     dy = boutdata.collect("dy", path=directory, info=False)
@@ -128,12 +106,6 @@ def request_data(pipeline):
 
     # VTK output object
     sgo = pipeline.GetStructuredGridOutput()
-    # print(sgo)
-
-    # Not sure if needed - taken from gs2 script
-    # if not hasattr(sgo, "GetProducerPort"):
-    #     print("Using vtkStructuredGrid")
-    #     sgo = vtkStructuredGrid()
 
     # Set size of output object
     sgo.SetDimensions(nx, ny, nz)
@@ -142,7 +114,7 @@ def request_data(pipeline):
     # Add coordinate points
     sgo.SetPoints(points)
 
-    with DataFile(file_list[0]) as f:
+    with DataFile(filename) as f:
         # Get list of variables with at least 2 dimensions
         var_list = [var for var in f.list() if f.ndims(var) > 2]
         # Get timesteps
@@ -160,11 +132,6 @@ def request_data(pipeline):
     # Add the variables to the output
     add_fields_to_sgo(sgo, var_list, directory, timestep=timestep)
 
-    # print(sgo)
-    # print(sgo.GetPoints())
-
-    # pipeline.SetStructuredGridOutput(sgo)
-    
     return pipeline
 
 def add_fields_to_sgo(sgo, var_list, directory, timestep=None):
@@ -175,13 +142,8 @@ def add_fields_to_sgo(sgo, var_list, directory, timestep=None):
         timestep = 0
 
     for var in var_list:
-        # VTK array to hold data
-        # vtk_data = vtkDoubleArray()
-
         # Read data
         bout_data = boutdata.collect(var, tind=timestep, path=directory, info=False)
-        # print(var)
-        # print(bout_data.shape)
 
         # Convert from numpy to vtk
         vtk_data = numpy_support.numpy_to_vtk(np.squeeze(bout_data).T.flatten().copy(), deep=1)
